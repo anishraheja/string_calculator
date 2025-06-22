37bae75c09d1897f2c65374c1c3e092e1396233a
anishraheja <anishraheja21@gmail.com>
2025-06-23 02:58:32 +0530

    Task 1: Added test cases - StringCalculator class responds to add class method - Empty input handling - Single value input handling

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -3,5 +3,20 @@ require_relative '../../lib/string_calculator'
 
 RSpec.describe StringCalculator do
   describe '.add' do
+    it 'responds to class method call' do
+      expect(StringCalculator).to respond_to(:add)
+    end
+
+    context 'when empty input is supplied' do
+      it 'returns 0 for an empty string' do
+        expect(StringCalculator.add('')).to eq(0)
+      end
+    end
+
+    context 'when single number is present in input' do
+      it 'returns the number itself if one number is provided' do
+        expect(StringCalculator.add('1')).to eq(1)
+      end
+    end
   end
 end
